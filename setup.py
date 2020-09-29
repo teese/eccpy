5a16c1398ecfc21afbdc839717c277aedf28f4b3
mark <mark.teese@tum.de>
2020-09-29 23:14:10 +0200

    fix license in files and setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-ECCpy is a user-friendly program for the high-throughput calculation of EC50 values.
-
-Copyright (C) 2016  Mark George Teese
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -30,7 +10,7 @@ with open(path.join(here, 'readme.rst')) as f:
 
 classifiers = """\
 Intended Audience :: Science/Research
-License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
+License :: OSI Approved :: MIT License
 Programming Language :: Python :: 3
 Topic :: Scientific/Engineering :: Bio-Informatics
 Topic :: Scientific/Engineering :: Medical Science Apps.
@@ -49,7 +29,7 @@ setup(name='eccpy',
                     "TNG Technology Consulting GmbH": "https://www.tngtech.com/en/index.html"},
       long_description=long_description,
       long_description_content_type='text/x-rst',
-      license='LGPLv3',
+      license='MIT',
       packages=find_packages(),
       classifiers=classifiers.splitlines(),
       install_requires=["pandas", "numpy", "matplotlib", "pytest", "scipy", "xlrd", "openpyxl", "pytest"],
